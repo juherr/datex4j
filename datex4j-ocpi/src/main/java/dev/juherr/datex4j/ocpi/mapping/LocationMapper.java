b1f1af9190cf1c5dc753f7a4a19892b0df24d3b1
Julien Herr <me@juherr.dev>
2026-07-23 22:04:09 +0200

    feat(ocpi): map OCPI location to DATEX II energy infrastructure site

---
+++ datex4j-ocpi/src/main/java/dev/juherr/datex4j/ocpi/mapping/LocationMapper.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2026 the datex4j authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.juherr.datex4j.ocpi.mapping;
+
+import dev.juherr.datex4j.model.v3_7.energyinfrastructure.EnergyInfrastructureSite;
+import dev.juherr.datex4j.model.v3_7.energyinfrastructure.EnergyInfrastructureStation;
+import dev.juherr.datex4j.ocpi.mapping.internal.MultilingualStrings;
+import dev.juherr.datex4j.ocpi.model.v2_3.EVSE;
+import dev.juherr.datex4j.ocpi.model.v2_3.Location;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Maps an OCPI {@link Location} to a DATEX II {@link EnergyInfrastructureSite} and back.
+ *
+ * <p><b>Unmapped fields.</b> OCPI {@code country_code}, {@code party_id}, {@code address},
+ * {@code city}, {@code postal_code}, {@code time_zone}, {@code opening_times}, {@code images},
+ * {@code directions}, {@code operator} are not mapped in this iteration; DATEX II {@code typeOfSite},
+ * {@code brand} have no OCPI equivalent.
+ */
+public final class LocationMapper {
+
+    private static final String DEFAULT_LANG = "en";
+
+    private final EvseMapper evseMapper = new EvseMapper();
+    private final GeoLocationMapper geoLocationMapper = new GeoLocationMapper();
+
+    /** Builds a DATEX II site from {@code location}, or {@code null} if {@code location} is null. */
+    public EnergyInfrastructureSite toDatex(Location location) {
+        if (location == null) {
+            return null;
+        }
+        EnergyInfrastructureSite site = new EnergyInfrastructureSite();
+        site.setId(location.getId());
+        site.setName(MultilingualStrings.of(DEFAULT_LANG, location.getName()));
+        site.setLocationReference(geoLocationMapper.toDatex(location.getCoordinates()));
+        if (location.getEvses() != null) {
+            location.getEvses()
+                    .forEach(evse -> site.getEnergyInfrastructureStation().add(evseMapper.toDatex(evse)));
+        }
+        return site;
+    }
+
+    /** Builds an OCPI location from {@code site}, or {@code null} if {@code site} is null. */
+    public Location toOcpi(EnergyInfrastructureSite site) {
+        if (site == null) {
+            return null;
+        }
+        Location location = new Location();
+        location.setId(site.getId());
+        location.setName(MultilingualStrings.firstValue(site.getName()));
+        location.setCoordinates(geoLocationMapper.toOcpi(site.getLocationReference()));
+        List<EVSE> evses = new ArrayList<>();
+        for (EnergyInfrastructureStation station : site.getEnergyInfrastructureStation()) {
+            evses.add(evseMapper.toOcpi(station));
+        }
+        location.setEvses(evses);
+        return location;
+    }
+}
